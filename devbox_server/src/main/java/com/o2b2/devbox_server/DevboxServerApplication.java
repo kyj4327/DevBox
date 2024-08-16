366e6cc2351e131a8a98ff1f64169269d9786308
youngjun kim <youngjun4327@gmail.com>
2024-08-16 14:26:57 +0900

    project 초기화

---
+++ devbox_server/src/main/java/com/o2b2/devbox_server/DevboxServerApplication.java
@@ -0,0 +1,13 @@
+package com.o2b2.devbox_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevboxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevboxServerApplication.class, args);
+	}
+
+}
---
+++ devbox_server/src/test/java/com/o2b2/devbox_server/DevboxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o2b2.devbox_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevboxServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
