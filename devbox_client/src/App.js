aee5c1b8654bc7cddf760553e02b3c1716b54c06
yerm <leeyelim98@gmail.com>
2024-08-20 13:32:49 +0900

    게시판 list 기본 틀 작성 #9

--- devbox_client/src/App.js
+++ devbox_client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
+import './assets/img/apple-icon.png';
+import './assets/img/favicon.ico';
+import './assets/css/bootstrap.min.css';
+import './assets/css/boxicon.min.css';
+import './assets/css/templatemo.css';
+import './assets/css/custom.css';
+
+import Header from './components/Header';
+import JobInfoList from './components/JobInfoList';
+import Footer from './components/Footer';
+
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <JobInfoList />
+      <Footer />
     </div>
   );
 }
