bc15e9fe317eca56ed5146047e8884acc37bbb07
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-08-21 14:31:32 +0900

    교육 상세 페이지 #18

--- devbox_server/src/main/java/com/o2b2/devbox_server/eduInfo/controller/EduController.java
+++ devbox_server/src/main/java/com/o2b2/devbox_server/eduInfo/controller/EduController.java
@@ -48,33 +48,33 @@ public class EduController {
 
 
 
-    // @GetMapping("/edu/list")
-    // @ResponseBody
-    // public List<Map<String, Object>> eduList() {
+    @GetMapping("/edu/list")
+    @ResponseBody
+    public List<Map<String, Object>> eduList() {
 
-    //     List<Map<String, Object>> list = new ArrayList<>();
-    //     Map<String, Object> map = new HashMap<>();
-    //     map.put("title", "1번 제목");
-    //     map.put("desc", "1번 내용");
-    //     list.add(map);
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "1번 제목");
+        map.put("desc", "1번 내용");
+        list.add(map);
 
-    //     map = new HashMap<>();
-    //     map.put("title", "2번 제목");
-    //     map.put("desc", "2번 내용");
-    //     list.add(map);
+        map = new HashMap<>();
+        map.put("title", "2번 제목");
+        map.put("desc", "2번 내용");
+        list.add(map);
 
-    //     return list;
-    // }
-    // @GetMapping("/edu/detail")
-    // @ResponseBody
-    // public Map<String, Object> eduDetail() {
+        return list;
+    }
+    @GetMapping("/edu/detail")
+    @ResponseBody
+    public Map<String, Object> eduDetail() {
 
-    //     Map<String, Object> map = new HashMap<>();
-    //     map.put("title", "1번 제목2222");
-    //     map.put("desc", "1번 내용22222");
-    //     map.put("imagePath", "http://localhost:8080/1.webp");
+        Map<String, Object> map = new HashMap<>();
+        map.put("title", "1번 제목2222");
+        map.put("desc", "1번 내용22222");
+        map.put("imagePath", "http://localhost:8080/1.webp");
 
-    //     return map;
-    // }
+        return map;
+    }
     
 }
