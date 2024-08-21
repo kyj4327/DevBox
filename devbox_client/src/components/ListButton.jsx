b7d7d074363046f9a64080e4913e0e5574b2b45f
yerm <leeyelim98@gmail.com>
2024-08-21 14:57:41 +0900

    버튼 component로 분리 #8

---
+++ devbox_client/src/components/ListButton.jsx
@@ -0,0 +1,10 @@
+const ListButton = ({ text, onClick }) => {
+    return (
+        <button type="submit" class="btn btn-secondary text-white px-md-4 px-2 py-md-3 py-1 radius-0 light-300"
+            onClick={onClick}>
+            {text}
+        </button>
+    );
+};
+
+export default ListButton;
