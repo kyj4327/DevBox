c7961aa4e23b1167e9f20566fc80fe41bbf64704
yerm <leeyelim98@gmail.com>
2024-08-22 14:14:26 +0900

    ReferenceList DB에 저장된 데이터 불러오기 #20

--- devbox_client/src/pages/ReferenceList.jsx
+++ devbox_client/src/pages/ReferenceList.jsx
@@ -3,12 +3,25 @@ import Pagination from '../components/Pagination';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
 import { useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const ReferenceList = () => {
     const navigate = useNavigate();
     const toWrite = () => {
         navigate('/reference/write');
     };
+
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        async function get() {
+            const url = 'http://127.0.0.1:8080/reference/list';
+            const res = await fetch(url);
+            const data = await res.json();
+            setData(data);
+        }
+        get();
+    }, []);
+
     return (
         <div>
             <Header />
@@ -27,67 +40,27 @@ const ReferenceList = () => {
                             <a className="filter-btn btn rounded-pill btn-outline-primary border-0 m-md-2 px-md-4" data-filter=".marketing" href="#">Others</a>
                         </div>
                     </div>
-                    <div class="pricing-list shadow-sm rounded-top rounded-3 py-sm-0 py-5">
-                        <div class="row p-2">
-                            <div class="pricing-list-icon col-3 text-center m-auto text-secondary ml-5 py-2">
-                                <h3>Coolors</h3>
-                            </div>
-                            <div class="pricing-list-body col-md-5 align-items-center pl-3 pt-2">
-                                <li style={{ listStyle: 'none' }}>기타</li>
-                                <li>색 조합 사이트</li>
-                                <li>회원가입하면 사용자가 색 설정 가능</li>
-                            </div>
-                            <div class="pricing-list-footer col-4 text-center m-auto align-items-center">
-                                <a href="https://coolors.co/" class="btn rounded-pill px-4 btn-primary light-300" target='_blank'>Link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-list shadow-sm rounded-top rounded-3 py-sm-0 py-5">
-                        <div class="row p-2">
-                            <div class="pricing-list-icon col-3 text-center m-auto text-secondary ml-5 py-2">
-                                <h3>News API</h3>
-                            </div>
-                            <div class="pricing-list-body col-md-5 align-items-center pl-3 pt-2">
-                                <li style={{ listStyle: 'none' }}>웹 개발</li>
-                                <li>헤드라인 News 기사를 가져올 수 있는 API</li>
-                                <li>카테고리, 언어, 나라별로 나눠져있어 다양하게 활용 가능</li>
-                            </div>
-                            <div class="pricing-list-footer col-4 text-center m-auto align-items-center">
-                                <a href="https://newsapi.org/" class="btn rounded-pill px-4 btn-primary light-300" target='_blank'>Link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-list shadow-sm rounded-top rounded-3 py-sm-0 py-5">
-                        <div class="row p-2">
-                            <div class="pricing-list-icon col-3 text-center m-auto text-secondary ml-5 py-2">
-                                <h3>gitignore</h3>
-                            </div>
-                            <div class="pricing-list-body col-md-5 align-items-center pl-3 pt-2">
-                                <li style={{ listStyle: 'none' }}>기타</li>
-                                <li>gitignore를 생성해주는 사이트</li>
-                                <li>키워드만 입력하면 자동으로 생성해줌</li>
-                            </div>
-                            <div class="pricing-list-footer col-4 text-center m-auto align-items-center">
-                                <a href="https://www.toptal.com/developers/gitignore/" class="btn rounded-pill px-4 btn-primary light-300" target='_blank'>Link</a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="pricing-list shadow-sm rounded-top rounded-3 py-sm-0 py-5">
-                        <div class="row p-2">
-                            <div class="pricing-list-icon col-3 text-center m-auto text-secondary ml-5 py-2">
-                                <h3>공공데이터포털</h3>
-                                수정 삭제
-                            </div>
-                            <div class="pricing-list-body col-md-5 align-items-center pl-3 pt-2">
-                                <li style={{ listStyle: 'none' }}>웹 개발</li>
-                                <li>공공기관이 만들어내는 모든 자료나 정보</li>
-                                <li>무료 오픈 API 찾을 수 있음</li>
-                            </div>
-                            <div class="pricing-list-footer col-4 text-center m-auto align-items-center">
-                                <a href="https://www.data.go.kr/" class="btn rounded-pill px-4 btn-primary light-300" target='_blank'>Link</a>
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        data.map((v) => {
+                            return (
+                                <div class="pricing-list shadow-sm rounded-top rounded-3 py-sm-0 py-5">
+                                    <div class="row p-2">
+                                        <div class="pricing-list-icon col-3 text-center m-auto text-secondary ml-5 py-2">
+                                            <h3>{v.title}</h3>
+                                        </div>
+                                        <div class="pricing-list-body col-md-5 align-items-center pl-3 pt-2">
+                                            <li style={{ listStyle: 'none' }}>{v.selectJob}</li>
+                                            <li>{v.content1}</li>
+                                            <li>{v.content2}</li>
+                                        </div>
+                                        <div class="pricing-list-footer col-4 text-center m-auto align-items-center">
+                                            <a href={v.link} class="btn rounded-pill px-4 btn-primary light-300" target='_blank'>Link</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <div class="form-row pt-2">
                     <div class="col-md-12 col-10 text-end">
