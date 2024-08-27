6c7e7f751b0ec5eaa99fdde03b8849acd3d05953
youngjun kim <youngjun4327@gmail.com>
2024-08-27 20:01:38 +0900

    권한 test용 컨트롤러 #7

---
+++ devbox_server/src/main/java/com/o2b2/devbox_server/user/controller/AdminController.java
@@ -0,0 +1,24 @@
+package com.o2b2.devbox_server.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminP() {
+
+        return "Admin Controller";
+    }
+
+    @GetMapping("/user")
+    public String userP() {
+
+        return "User Controller";
+    }
+
+
+}
