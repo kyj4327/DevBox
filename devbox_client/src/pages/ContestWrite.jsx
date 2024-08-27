52f5171285f6092cb0cac0ffb74b0c65d15905ec
yerm <leeyelim98@gmail.com>
2024-08-27 14:34:07 +0900

    ContestWrite에 WriteShort 컴포넌트 적용 #49

--- devbox_client/src/pages/ContestWrite.jsx
+++ devbox_client/src/pages/ContestWrite.jsx
@@ -1,5 +1,6 @@
 import Header from '../components/Header';
 import WriteLong from '../components/WriteLong';
+import WriteShort from '../components/WriteShort';
 import Footer from '../components/Footer';
 import Button from '../components/Button';
 import { useNavigate } from 'react-router-dom';
@@ -47,28 +48,13 @@ const ContestWrite = () => {
                     <p class="text-center pb-5 light-300">대회/공모전의 세부요강은 주최사의 기획에 의해 내용이 변경될 수 있으니, 주최사의 공고를 반드시 확인해 보시기 바랍니다.</p>
                     <div class="pricing-list rounded-top rounded-3 py-sm-0 py-5">
                         <div class="contact-form row">
-                            <WriteLong titleTag={'공모명'} name={'title'} value={title} onChange={(e) => { setTitle(e.target.value) }} />
-                            <WriteLong titleTag={'주최/주관'} name={'host'} value={host} onChange={(e) => { setHost(e.target.value) }} />
-                            <WriteLong titleTag={'참가대상'} name={'target'} value={target} onChange={(e) => { setTarget(e.target.value) }} />
-                            <h2 class="worksingle-heading h3 pb-3 light-300 typo-space-line">접수기간</h2>
-                            <p class="worksingle-footer py-3 text-muted light-300" style={{ display: 'flex' }}>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="form-floating">
-                                        <input type="date" class="form-control form-control-lg light-300" id="regStart" name="regStart" placeholder="시작"
-                                            value={regStart} onChange={(e) => { setRegStart(e.target.value) }} />
-                                        <label for="floatingsubject light-300">시작</label>
-                                    </div>
-                                </div>
-                                <div class="col-lg-6 mb-4">
-                                    <div class="form-floating">
-                                        <input type="date" class="form-control form-control-lg light-300" id="regEnd" name="regEnd" placeholder="마감"
-                                            value={regEnd} onChange={(e) => { setRegEnd(e.target.value) }} />
-                                        <label for="floatingsubject light-300">마감</label>
-                                    </div>
-                                </div>
-                            </p>
-                            <WriteLong titleTag={'공식 홈페이지 주소'} name={'officialUrl'} value={officialUrl} onChange={(e) => { setOfficialUrl(e.target.value) }} />
-                            <WriteLong titleTag={'이미지 주소'} name={'imgUrl'} value={imgUrl} onChange={(e) => { setImgUrl(e.target.value) }} />
+                            <WriteLong type={'text'} titleTag={'공모명'} name={'title'} value={title} onChange={(e) => { setTitle(e.target.value) }} />
+                            <WriteLong type={'text'} titleTag={'주최/주관'} name={'host'} value={host} onChange={(e) => { setHost(e.target.value) }} />
+                            <WriteLong type={'text'} titleTag={'참가대상'} name={'target'} value={target} onChange={(e) => { setTarget(e.target.value) }} />
+                            <WriteShort type={'date'} titleTag={'접수시작'} name={'regStart'} value={regStart} onChange={(e) => { setRegStart(e.target.value) }} />
+                            <WriteShort type={'date'} titleTag={'접수마감'} name={'regEnd'} value={regEnd} onChange={(e) => { setRegEnd(e.target.value) }} />
+                            <WriteLong type={'text'} titleTag={'공식 홈페이지 주소'} name={'officialUrl'} value={officialUrl} onChange={(e) => { setOfficialUrl(e.target.value) }} />
+                            <WriteLong type={'text'} titleTag={'이미지 주소'} name={'imgUrl'} value={imgUrl} onChange={(e) => { setImgUrl(e.target.value) }} />
                         </div>
                     </div>
                 </div>
