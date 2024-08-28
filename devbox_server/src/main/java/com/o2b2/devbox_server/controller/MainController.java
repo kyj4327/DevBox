fc17795ef1994e089f2523153e2df0fa4206c8de
youngjun kim <youngjun4327@gmail.com>
2024-08-28 21:07:31 +0900

    ✨feat: 회원정보 출력 컨트롤러 삭제 #7

--- devbox_server/src/main/java/com/o2b2/devbox_server/controller/MainController.java
+++ devbox_server/src/main/java/com/o2b2/devbox_server/controller/MainController.java
@@ -1,15 +1,9 @@
 package com.o2b2.devbox_server.controller;
 
-import com.o2b2.devbox_server.user.dto.CustomOAuth2User;
-import com.o2b2.devbox_server.user.dto.UserDTO;
-import com.o2b2.devbox_server.user.entity.UserEntity;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import com.o2b2.devbox_server.user.repository.UserRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
@@ -18,6 +12,14 @@ import java.util.Map;
 @Controller
 @ResponseBody
 public class MainController {
+
+
+    private final UserRepository userRepository;
+
+    public MainController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 
     @GetMapping("/")
     public String mainP() {
@@ -32,30 +34,17 @@ public class MainController {
         return "my route";
     }
 
-//    @GetMapping("/api/user/me")
-//    public Map<String, Object> getCurrentUser(@AuthenticationPrincipal OAuth2User oauth2User) {
-//        Map<String, Object> userInfo = new HashMap<>();
-//
-//        if (oauth2User != null) {
-//            userInfo.put("name", oauth2User.getAttribute("name"));
-//            userInfo.put("email", oauth2User.getAttribute("email"));
-//            userInfo.put("nickname", oauth2User.getAttribute("nickname"));
-//        } else {
-//            userInfo.put("error", "User is not authenticated");
-//        }
-//
-//        return userInfo;
-//    }
-//}
+    @GetMapping("/api/user/me")
+    public Map<String, Object> getCurrentUser(Authentication authentication) {
+        Map<String, Object> userInfo = new HashMap<>();
+        if (authentication != null && authentication.isAuthenticated()) {
+            // authentication에서 사용자 정보 추출
+            // userInfo에 정보 추가
+        } else {
+            userInfo.put("error", "User is not authenticated");
+        }
+        return userInfo;
+    }
 
-//    // 유저 정보 조회
-//    @GetMapping("/api/user/me")
-//    public ResponseEntity<UserDTO> getCurrentUser(@AuthenticationPrincipal UserEntity userEntity) {
-//        if (userEntity != null) {
-//            UserDTO userDTO = new UserDTO(userEntity.getEmail(),userEntity.getName(),userEntity.getRole(), userEntity.getNickname());
-//            return ResponseEntity.ok(userDTO);
-//        } else {
-//            return ResponseEntity.badRequest().build();
-//        }
-//    }
 }
+
