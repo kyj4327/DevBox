f7fdb27283cbf2b94b671c59de301448b3949a36
yerm <leeyelim98@gmail.com>
2024-08-29 15:52:07 +0900

    a 태그 router Link로 수정 #8

--- devbox_client/src/components/JobInfo.jsx
+++ devbox_client/src/components/JobInfo.jsx
@@ -2,14 +2,16 @@ import { Link } from "react-router-dom";
 
 const JobInfo = ({ jobinfo, src, alt, text }) => {
     return (
-        <Link to={`/jobinfo/${jobinfo}`} className="col-sm-6 col-lg-4 text-decoration-none">
-            <div className="service-work overflow-hidden card mb-5 mx-5 m-sm-0">
-                <img className="card-img-top" src={src} alt={alt} />
-                <div className="card-body">
-                    <h3 className="card-title light-300 text-dark">{text}</h3>
+        <div className="col-sm-6 col-lg-4" style={{ marginBottom: '3rem' }}>
+            <Link to={`/jobinfo/${jobinfo}`} className="text-decoration-none">
+                <div className="service-work overflow-hidden card mb-5 mx-5 m-sm-0">
+                    <img className="card-img-top" src={src} alt={alt} />
+                    <div className="card-body">
+                        <h3 className="card-title light-300 text-dark">{text}</h3>
+                    </div>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 };
 
