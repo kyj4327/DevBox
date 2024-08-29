bee76e709631dea673ccf2807b37babbf8e097e0
youngjun kim <youngjun4327@gmail.com>
2024-08-29 16:07:49 +0900

    ♻️refactor: 코드 정리 #7

--- devbox_server/src/main/java/com/o2b2/devbox_server/user/dto/UserDTO.java
+++ devbox_server/src/main/java/com/o2b2/devbox_server/user/dto/UserDTO.java
@@ -16,14 +16,4 @@ public class UserDTO {
     private String provider; // google, naver, kakao 인지 어느 소셜 로그인했는지
     private String providerId; // 소셜 자체 id
 
-//    public UserDTO(String email,String name, String role, String nickname) {
-//    }
-
-//    public UserDTO(String email, String name,String role, String nickname) {
-//        this.email = email;
-//        this.role = role;
-//        this.name = name;
-//        this.username = username;
-//        this.nickname = nickname;
-//    }
 }
