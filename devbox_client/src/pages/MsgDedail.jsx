8d2a0ef35c926a24a7cfbf9cff13f9dba1cbc18b
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-09-04 17:47:14 +0900

    쪽지 상세 디자인 #98

--- devbox_client/src/pages/MsgDedail.jsx
+++ devbox_client/src/pages/MsgDedail.jsx
@@ -30,33 +30,49 @@ const MesDetail = () => {
     }
 
     return (
-        <div className="MesDetail">
-            <div class="row pb-4">
-                <div class="worksingle-comment-body col-md-8 m-auto">
-                    <div class="d-flex">
-                        <div class="comment-body">
-                            <div class="comment-header d-flex justify-content-between ms-3">
-                                <div class="header text-start">
-                                    <h5 class="h5">보낸사람: {msgData.sender}</h5>
-                                    <h5 class="h5">받는사람: {msgData.reciver}</h5>
+        <div>
+            <section class="container py-5">
+                <div className="container py-5">
+                    <h1 className="h2 semi-bold-600 text-center mt-2 pb-5 ">쪽지</h1>
+                    <div className="pricing-list rounded-top rounded-3 py-sm-0 py-5">
+
+                        <div class="row pb-4">
+                            <div class="worksingle-comment-body col-md-8 m-auto">
+                                <div class="d-flex">
+                                    <div class="comment-body">
+                                        <div class="comment-header d-flex justify-content-between ms-3">
+                                            <div class="header text-start">
+                                                <h5 class="h5 my-4">보낸사람: {msgData.sender}</h5>
+                                                <h5 class="h5 my-4">받는사람: {msgData.reciver}</h5>
+                                            </div>
+                                        </div>
+                                        <div class="comment-header d-flex justify-content-between ms-3">
+                                            <h5 class="h6 my-2">제목: {msgData.title}</h5>
+                                        </div>
+                                        <div class="footer">
+                                            <div class="card-body border ms-3 light-300">
+                                                {msgData.content}
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <a href="#" class="text-decoration-none text-secondary"><i class='bx bxs-share me-2'></i>Reply</a>
-                            </div>
-                            <div class="comment-header d-flex justify-content-between ms-3">
-                                <h5 class="h6">{msgData.title}</h5>
-                            </div>
-                            <div class="footer">
-                                <div class="card-body border ms-3 light-300">
-                                   {msgData.content}
+
+
+                                <div className="row pt-5">
+                                    <div className="col text-start">
+
+                                        <Button text={'목록'} onClick={(e) => { e.preventDefault(); navigate('/message/list') }} />
+                                    </div>
+                                    <div className="col text-end">
+
+                                        <Button text={'삭제'} onClick={deleteState} />
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <Button text={'목록'} onClick={(e) => {e.preventDefault(); navigate('/message/list') }} />
-            <Button text={'삭제'} onClick={deleteState} />
+            </section>
         </div>
     );
 };
