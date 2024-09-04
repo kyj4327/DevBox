c712cf9b2fcfc32e9da504c40e9de102769c0dd6
yerm <leeyelim98@gmail.com>
2024-09-04 11:01:38 +0900

    예약 백엔드 기본 기능 구현 #88

---
+++ devbox_server/src/main/java/com/o2b2/devbox_server/reservation/controller/ReservationController.java
@@ -0,0 +1,31 @@
+package com.o2b2.devbox_server.reservation.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.o2b2.devbox_server.reservation.model.Reservation;
+import com.o2b2.devbox_server.reservation.repository.ReservationRepository;
+
+@RestController
+@CrossOrigin
+public class ReservationController {
+    @Autowired
+    ReservationRepository reservationRepository;
+
+    @PostMapping("/reservation")
+    public Map<String, Object> reservation(@RequestBody Reservation reservation) {
+        Reservation result = reservationRepository.save(reservation);
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 200);
+        map.put("msg", "입력 완료");
+        map.put("result", result);
+        return map;
+    }
+
+}
