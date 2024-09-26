df9fc5cbc1f7d5d555e97a4cdae9fadaf9a69487
yerm <leeyelim98@gmail.com>
2024-09-26 15:08:19 +0900

    Header 구현 #202

--- devbox_client/src/components/Header.jsx
+++ devbox_client/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import MsgBell from "../pages/message/MsgBell";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useUser } from '../components/context/UserContext';
 import './Header.css';
 
@@ -14,7 +14,7 @@ const Header = () => {
         method: "POST",
         credentials: "include",
         headers: {
-          "Content-Type": "application/json", 
+          "Content-Type": "application/json",
           "Authorization": `Bearer ${localStorage.getItem('accessToken')}`,
         },
       });
@@ -32,99 +32,75 @@ const Header = () => {
   };
 
   return (
-    <nav
-      id="main_nav"
-      className="navbar navbar-expand-lg navbar-light bg-white shadow"
-    >
+    <nav id="main_nav" className="navbar navbar-expand-lg navbar-light bg-white shadow">
       <div className="container d-flex justify-content-between align-items-center">
-        <a className="navbar-brand h1" href="/home">
+        <Link to="/" className="navbar-brand h1">
           <i className="bx bx-buildings bx-sm text-dark"></i>
           <span className="text-dark h4">Dev</span>
           <span className="text-primary h4">Box</span>
-        </a>
-        <button
-          className="navbar-toggler border-0"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbar-toggler-success"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between"
-          id="navbar-toggler-success"
-        >
-          <div className="flex-fill mx-xl-5 mb-2">
+        </Link>
+        <div className="align-self-center collapse navbar-collapse flex-fill d-lg-flex justify-content-lg-between" id="navbar-toggler-success">
+          <div className="flex-fill mx-xl-5 mb-2 menu-container">
             <ul className="nav navbar-nav d-flex justify-content-between mx-xl-5 text-center text-dark">
               <li className="nav-item">
-                <a
-                  className="nav-link btn-outline-primary rounded-pill px-3"
-                  href="index.html"
-                >
-                  BDIA
-                </a>
+                <span className="nav-link btn-outline-primary rounded-pill px-3" style={{ cursor: 'default' }}>BDIA</span>
+                <div className="dropdown-menu-container">
+                  <div className="dropdown-section">
+                    <Link to="/introduce">BDIA 소개</Link>
+                    <Link to="/schedule">연간 교육 일정</Link>
+                    <Link to="/edu/list">교육 과정</Link>
+                  </div>
+                </div>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link btn-outline-primary rounded-pill px-3"
-                  href="about.html"
-                >
-                  Notice
-                </a>
+                <Link to="/notice/list" className="nav-link btn-outline-primary rounded-pill px-3">공지사항</Link>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link btn-outline-primary rounded-pill px-3"
-                  href="/edu/list"
-                >
-                  Information
-                </a>
+                <span className="nav-link btn-outline-primary rounded-pill px-3" style={{ cursor: 'default' }}>정보마당</span>
+                <div className="dropdown-menu-container">
+                  <div className="dropdown-section">
+                    <Link to="/jobinfo/list">개발 직군</Link>
+                    <Link to="/hiring/list">채용 공고</Link>
+                    <Link to="/contest/list">공모전 공고</Link>
+                  </div>
+                </div>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link btn-outline-primary rounded-pill px-3"
-                  href="/project/list"
-                >
-                  Community
-                </a>
+                <span className="nav-link btn-outline-primary rounded-pill px-3" style={{ cursor: 'default' }}>커뮤니티</span>
+                <div className="dropdown-menu-container">
+                  <div className="dropdown-section">
+                    <Link to="/greeting/list">가입인사</Link>
+                    <Link to="/freeboard/list">자유게시판</Link>
+                    <Link to="/project/list">프로젝트 자랑</Link>
+                    <Link to="/gathermate/list">모여라 메이트</Link>
+                    <Link to="/reference/list">추천해요</Link>
+                    <Link to="/reservation/write">6층 회의실 대여</Link>
+                  </div>
+                </div>
               </li>
               <li className="nav-item">
-                <a
-                  className="nav-link btn-outline-primary rounded-pill px-3"
-                  href=""
-                >
-                  FAQ
-                </a>
+                <Link to="/faq" className="nav-link btn-outline-primary rounded-pill px-3">FAQ</Link>
               </li>
             </ul>
           </div>
           <div className="navbar align-self-center d-flex">
-            {user ? (
-              <>
-                <MsgBell />
-                <a className="nav-link" href="/mypage">
-                  <i className="bx bx-user-circle bx-sm text-primary"></i>
-                </a>
-                <button 
-                  onClick={handleLogoutClick} 
-                  className="header-logout-button nav-link"
-                  aria-label="Logout"
-                >
-                  <i className='bx bx-log-out bx-sm text-primary'></i>
-                </button>
-              </>
-            ) : (
-              <a
-                className="nav-link"
-                href="/auth"
-                style={{ color: "#4232C2", textDecoration: 'underline', cursor: 'pointer' }}
-              >
-                로그인/회원가입
-              </a>
-            )}
+            {
+              user ? (
+                <>
+                  <MsgBell />
+                  <Link to="/mypage" className="nav-link">
+                    <i className="bx bx-user-circle bx-sm text-primary"></i>
+                  </Link>
+                  <button onClick={handleLogoutClick} className="header-logout-button nav-link" aria-label="Logout">
+                    <i className='bx bx-log-out bx-sm text-primary'></i>
+                  </button>
+                </>
+              ) : (
+                <Link to="/auth" className="nav-link" style={{ color: "#4232C2", textDecoration: 'underline', cursor: 'pointer' }}>
+                  로그인/회원가입
+                </Link>
+              )
+            }
           </div>
         </div>
       </div>
