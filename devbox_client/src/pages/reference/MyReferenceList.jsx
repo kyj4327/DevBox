1a1cb44b806e32613fb9e9db6a3eeeca83802e6b
yerm <leeyelim98@gmail.com>
2024-09-27 16:18:13 +0900

    Client : reference 내가 쓴 글 불러오기 #193

---
+++ devbox_client/src/pages/reference/MyReferenceList.jsx
@@ -0,0 +1,124 @@
+import Pagination from '../../components/Pagination';
+import { Link, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useUser } from '../../components/context/UserContext';
+
+const MyReferenceList = () => {
+    const { user } = useUser();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (!user) {
+            alert("로그인이 필요합니다.");
+            navigate('/auth');
+        }
+    }, [user, navigate]);
+    const token = localStorage.getItem('accessToken');
+
+    const userNickName = user ? user.nickname : null;
+
+    const [selectJob, setSelectJob] = useState('All');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [data, setData] = useState([]);
+    const [pageData, setPageData] = useState([]);
+    useEffect(() => {
+        async function get(page = 1) {
+            const url = `http://localhost:8080/reference/mylist/${selectJob}?page=${page}`;
+            const res = await fetch(url, {
+                credentials: 'include',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            });
+            const data = await res.json();
+            const listData = data.slice(0, -1);
+            const pageInfo = data[data.length - 1];
+            setData(listData);
+            setPageData(pageInfo);
+            setCurrentPage(page);
+        }
+        get(currentPage);
+    }, [selectJob, currentPage]);
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [data]);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+
+    return (
+        <div>
+            <section className="container py-5">
+                <div className="container py-5">
+                    <h1 className="h2 semi-bold-600 text-center mt-2">추천해요 mylist</h1>
+                    {
+                        data.map((v) => {
+                            return (
+                                <div className="pricing-list shadow-sm rounded-top rounded-3 py-sm-0 py-5" key={v.id}>
+                                    <div className="row p-2">
+                                        <div className="pricing-list-icon col-3 text-center m-auto text-secondary ml-5 py-2">
+                                            <h3>{v.title}</h3>
+                                            <span>작성자 : {v.userId}</span>
+                                        </div>
+                                        <div className="pricing-list-body col-md-5 align-items-center pl-3 pt-2">
+                                            <li style={{ listStyle: 'none' }}>{v.selectJob}</li>
+                                            <li>{v.content1}</li>
+                                            <li>{v.content2}</li>
+                                            {v.content3 === '' ? '' : <li>{v.content3}</li>}
+                                            {v.content4 === '' ? '' : <li>{v.content4}</li>}
+                                            {v.content5 === '' ? '' : <li>{v.content5}</li>}
+                                        </div>
+                                        <div className="pricing-list-footer col-4 text-center m-auto align-items-center">
+                                            <Link to={v.link} className="btn rounded-pill px-4 btn-primary light-300" target='_blank' style={{ marginRight: '1rem' }}>Link</Link>
+                                            {
+                                                v.userId === userNickName
+                                                    ? <>
+                                                        <Link style={{ marginRight: '0.5em', textDecoration: 'none' }}
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+                                                                navigate(`/reference/update?referenceId=${v.id}`);
+                                                            }}>수정</Link>
+                                                        <Link style={{ textDecoration: 'none' }}
+                                                            onClick={(e) => {
+                                                                e.preventDefault();
+                                                                if (!user) {
+                                                                    alert("로그인이 필요합니다.");
+                                                                    return;
+                                                                }
+                                                                const token = localStorage.getItem('accessToken');
+                                                                if (window.confirm("삭제하시겠습니까?")) {
+                                                                    async function send() {
+                                                                        const url = `http://localhost:8080/reference/delete?referenceId=${v.id}`;
+                                                                        await fetch(url, {
+                                                                            credentials: 'include',
+                                                                            headers: {
+                                                                                'Authorization': `Bearer ${token}`
+                                                                            }
+                                                                        });
+                                                                        alert("삭제되었습니다.");
+                                                                        window.location.reload();
+                                                                    }
+                                                                    send();
+                                                                }
+                                                            }}>삭제</Link>
+                                                    </>
+                                                    : <>
+                                                        <span style={{ visibility: 'hidden', marginRight: '0.5em', cursor: 'default' }}>수정</span>
+                                                        <span style={{ visibility: 'hidden', cursor: 'default' }}>삭제</span>
+                                                    </>
+                                            }
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </section>
+            <Pagination handlePageChange={handlePageChange} pageData={pageData} />
+        </div>
+    );
+};
+
+export default MyReferenceList;
