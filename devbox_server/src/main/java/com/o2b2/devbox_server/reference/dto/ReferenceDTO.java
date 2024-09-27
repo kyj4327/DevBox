e1e8d653fb57937f1934b002a6c3baa6a7751d37
youngjun kim <youngjun4327@gmail.com>
2024-09-27 17:31:54 +0900

    1

---
+++ devbox_server/src/main/java/com/o2b2/devbox_server/reference/dto/ReferenceDTO.java
@@ -0,0 +1,22 @@
+package com.o2b2.devbox_server.reference.dto;
+
+import lombok.Data;
+
+@Data
+public class ReferenceDTO {
+    String title;
+
+    String selectJob;
+
+    String content1;
+
+    String content2;
+
+    String content3;
+
+    String content4;
+
+    String content5;
+
+    String link;
+}
