717d5a02f53763871f84769d83b5b89f92b18d3f
youngjun kim <youngjun4327@gmail.com>
2024-09-30 16:11:42 +0900

    ✨feat: 마이페이지 sidebar 하위 메뉴 구성 _클릭시 부메뉴 #241

--- devbox_client/src/components/MyPageSideBar.jsx
+++ devbox_client/src/components/MyPageSideBar.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { NavLink, useNavigate } from 'react-router-dom';
-import './MyPageSideBar.css';
-import profilePic from '../assets/img/profilePic.png';
+import React, { useEffect, useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import "./MyPageSideBar.css";
+import profilePic from "../assets/img/profilePic.png";
 
 function MyPageSideBar() {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,34 +15,38 @@ function MyPageSideBar() {
   }, []);
 
   const fetchUserData = async () => {
-    const response = await fetch('http://localhost:8080/api/user/me', {
-      method: 'GET',
-      credentials: 'include',
+    const response = await fetch("http://localhost:8080/api/user/me", {
+      method: "GET",
+      credentials: "include",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
       },
     });
-  
+
     if (response.ok) {
       const data = await response.json();
       setUser(data);
     } else {
       if (response.status === 401) {
-        navigate('/auth');
+        navigate("/auth");
       }
     }
   };
 
   const getRoleDisplayName = (role) => {
     switch (role) {
-      case 'ROLE_USER':
-        return '일반회원';
-      case 'ROLE_STUDENT':
-        return '수강생';
+      case "ROLE_USER":
+        return "일반회원";
+      case "ROLE_STUDENT":
+        return "수강생";
       default:
-        return ' ';
+        return " ";
     }
+  };
+
+  const toggleSubmenu = () => {
+    setIsSubmenuOpen(!isSubmenuOpen);
   };
 
   if (error) return <div>Error: {error}</div>;
@@ -48,13 +54,15 @@ function MyPageSideBar() {
   return (
     <div className="sidebar_profile mt-50">
       <div className="profile_user">
-      <div className="user_author">
-  <img src={profilePic} alt="author" />
-  <div className="user_content">
-    <h6 className="author_name">{user ? user.name : 'Name'}</h6>
-    <p className="user_role">{user ? getRoleDisplayName(user.role) : ''}</p>
-  </div>
-</div>
+        <div className="user_author">
+          <img src={profilePic} alt="author" />
+          <div className="user_content">
+            <h6 className="author_name">{user ? user.name : "Name"}</h6>
+            <p className="user_role">
+              {user ? getRoleDisplayName(user.role) : ""}
+            </p>
+          </div>
+        </div>
         <div className="user_list">
           <ul>
             <li>
@@ -68,19 +76,32 @@ function MyPageSideBar() {
               </NavLink>
             </li>
             <li>
-              <NavLink to="/mypage/retrospect">
-                <i className="fal fa-cog"></i> 회고록
-              </NavLink>
-            </li>
-            <li>
               <NavLink to="/mypage/reservation/check">
                 <i className="fal fa-layer-group"></i> 회의실 예약내역
               </NavLink>
             </li>
-            <li>
-              <NavLink to="/mypage/posts">
+            <li className={`has-submenu ${isSubmenuOpen ? "open" : ""}`}>
+              <button
+                type="button"
+                className="submenu-toggle"
+                onClick={toggleSubmenu}
+                aria-haspopup="true"
+                aria-expanded={isSubmenuOpen}
+              >
                 <i className="fal fa-envelope"></i> 내가쓴 글
-              </NavLink>
+              </button>
+              <ul className="submenu">
+                <li>
+                  <NavLink to="/mypage/gathermate/mylist">
+                    <i className="fal fa-envelope"></i> - 모여라 메이트
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/mypage/gathermate/mylists">
+                    <i className="fal fa-envelope"></i> - 추천해요
+                  </NavLink>
+                </li>
+              </ul>
             </li>
           </ul>
         </div>
