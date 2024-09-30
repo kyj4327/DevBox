2ecfa2e01e6113b4feb2ebcc8ccb5402d5e36bf8
MinJunKim <167392999+km8026@users.noreply.github.com>
2024-09-30 15:17:52 +0900

    프로젝트 좋아요 기능 오류 수정 #238

--- devbox_client/src/pages/project/ProjectDetail.jsx
+++ devbox_client/src/pages/project/ProjectDetail.jsx
@@ -11,18 +11,21 @@ import { FreeMode, EffectCoverflow, Autoplay, Pagination } from 'swiper/modules'
 import Swal from "sweetalert2";
 import { useUser } from "../../components/context/UserContext";
 import profilePic from "../../assets/img/profilePic.png";
+import UserContact from "../../components/UserContact";
 
 
 const ProjectDetail = () => {
-    const { user } = useUser();
+    const { user} = useUser();
     const navigate = useNavigate();
     const [proData, setProData] = useState({});
+    const [isHovered, setIsHovered] = useState(false);
     const [likeStatus, setLikeStatus] = useState({});
     const [refresh, setRefresh] = useState(true);
     const location = useLocation();
     const searchParams = new URLSearchParams(location.search);
     const id = searchParams.get('id');
     const domain = "http://localhost:8080";
+   
     const formatDateTime = (dateString) => {
         const date = new Date(dateString);
         const year = date.getFullYear();
@@ -38,6 +41,9 @@ const ProjectDetail = () => {
         const res = await fetch(`${domain}/project/detail?id=${id}`);
         const data = await res.json();
         setProData(data);
+        if (data && data.name) {
+            console.log(data.name);
+        }   
     }
 
     const fetchUserLikeStatus = async () => {
@@ -90,11 +96,19 @@ const ProjectDetail = () => {
 
     };
 
+    const handleLikeClick = (e, proId) => {
+        e.stopPropagation();
+        e.preventDefault();
+        likeCount(proId); // 좋아요 상태 업데이트
+    };
+
     useEffect(() => {
         getProjectDetail();
         fetchUserLikeStatus();
-    }, []);
-
+    }, []); 
+    
+    
+    
     return (
         <div className="container-full">
             <div className="container py-5">
@@ -172,32 +186,14 @@ const ProjectDetail = () => {
                     <div className="card-body">
                         <div className='d-flex justify-content-center'>
                         <div className="d-flex">
-                            <svg
-                                onClick={(e) => {
-                                    e.stopPropagation();
-                                    e.preventDefault();
-                                    likeCount(proData.id);
-                                }}
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="25"
-                                height="25"
-                                fill="currentColor"
-                                className="bx me-1"
-                                viewBox="0 0 16 16"
-                            >
-                                {likeStatus[proData.id] ? (
-                                    likeStatus[proData.id].liked ? (
-                                        <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
-                                    ) : (
-                                        <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                                    )
-                                ) : (
-
-                                    <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                                )
-                                }
-                            </svg>
-                            <span>{likeStatus[proData.id] !== undefined ? likeStatus[proData.id].count : proData.likeCount}</span>
+                                <i
+                                    className={`bx h2 px-1 ${likeStatus[proData.id]?.liked ? 'bxs-like' : 'bx-like'} ${isHovered ? 'bx-tada' : ''}`}
+                                    onClick={(e) => handleLikeClick(e, proData.id)} // 좋아요 클릭 시 처리
+                                    style={{ color: '#4232C2' }}
+                                    onMouseEnter={() => setIsHovered(true)}
+                                    onMouseLeave={() => setIsHovered(false)}
+                                ></i>
+                            <span className='pt-2 h4'style={{ color: '#4232C2' }}>{likeStatus[proData.id] !== undefined ? likeStatus[proData.id].count : proData.likeCount}</span>
                         </div>
                         </div>
                     </div>
@@ -217,7 +213,7 @@ const ProjectDetail = () => {
                                     }}
                                 />
                                 <div className="d-flex flex-column px-2">
-                                    <span>작성자: {proData.name}</span>
+                                    <span>작성자: <UserContact nickname={proData.name} /> </span>
                                     <span>작성일: {formatDateTime(proData.time)}</span>
                                 </div>
                             </div>
@@ -237,7 +233,7 @@ const ProjectDetail = () => {
                                     <Button text={'목록'} onClick={() => { navigate('/project/list') }} />
                                 </div>
                                 <div className="col text-end">
-                                    {proData.user && user && user.id === proData.user.id && (
+                                    {user && proData.name === user.nickname && (
                                         <>
                                             <button
                                                 type="button"
--- devbox_client/src/pages/project/ProjectMain.jsx
+++ devbox_client/src/pages/project/ProjectMain.jsx
@@ -3,11 +3,13 @@ import Button from '../../components/Button';
 import { useEffect, useState } from 'react';
 import { useUser } from '../../components/context/UserContext';
 import UserContact from '../../components/UserContact';
+import './ProjectMain.css';
 
 const ProjectMain = (props) => {
     const { user } = useUser();
     const navigate = useNavigate();
-    const [likeStatus, setLikeStatus] = useState({});
+    const [isHovered, setIsHovered] = useState(false); // 마우스 호버 상태 관리
+    const [likeStatus, setLikeStatus] = useState({}); // 좋아요 상태 및 수 관리
     const domain = "http://localhost:8080";
 
     const fetchUserLikeStatus = async () => {
@@ -34,7 +36,6 @@ const ProjectMain = (props) => {
         }
     };
 
-
     const likeCount = async (proId) => {
         const token = localStorage.getItem('accessToken');
         const url = `${domain}/project/like?id=${proId}`;
@@ -55,9 +56,13 @@ const ProjectMain = (props) => {
                 }
             }));
         }
-
-        // 상태 갱신을 위해 필요 시 추가        
         props.setRefresh();
+    };
+
+    const handleLikeClick = (e, proId) => {
+        e.stopPropagation();
+        e.preventDefault();
+        likeCount(proId); // 좋아요 상태 업데이트
     };
 
     useEffect(() => {
@@ -73,70 +78,50 @@ const ProjectMain = (props) => {
             setLikeStatus(initialLikeStatus);
         }
     }, [props.list]);
+
     return (
         <div className="container py-5">
             <h1 className="h2 semi-bold-600 text-center mt-2">프로젝트 자랑 게시판</h1>
             <p
-                        className="text-center light-300"
-                        style={{ marginBottom: "0", padding: "0px" }}
-                    >
-                        맘껏 자랑해봐!
-                    </p>
+                className="text-center light-300"
+                style={{ marginBottom: "0", padding: "0px" }}
+            >
+                맘껏 자랑해봐!
+            </p>
             <section className="container py-5">
                 <div className="row projects gx-lg-5">
                     {props.list && props.list.map((pro) => (
-                        <Link to={`/project/detail?id=${pro.id}`} key={pro.id} className="col-sm-6 col-lg-4 text-decoration-none project marketing social business">
-                            <div className="service-work overflow-hidden card mb-5 mx-2 ">
+                        <div className="service-work overflow-hidden card mb-3 mx-2 col-sm-6 col-lg-4 pt-4 project marketing social business" key={pro.id}>
+                            <Link to={`/project/detail?id=${pro.id}`} className="overflow-hidden card mb-2 mx-1 text-decoration-none">
                                 <img
                                     style={{ height: '400px' }}
                                     className="card-img-top"
                                     src={`${domain}/project/download?id=${pro.mainImg}`}
                                     alt={`Project ${pro.id}`}
                                 />
-                                <div className="card-body">
-                                    <h5 className="card-title h6 m-0 semi-bold-600 text-dark">{pro.title}</h5>
-                                    <p className="card-text pt-2 mb-1 light-300 text-dark">  
-
-                                        <UserContact nickname={pro.name} />
-                                        </p>
-                                    <div className='d-flex justify-content-end'>
-                                        <svg
-                                            onClick={(e) => {
-                                                e.stopPropagation();
-                                                e.preventDefault();
-                                                likeCount(pro.id);
-                                            }}
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            width="25"
-                                            height="25"
-                                            fill="currentColor"
-                                            className="bx me-1"
-                                            viewBox="0 0 16 16"
-                                        >
-                                            {likeStatus[pro.id] ? (
-                                                likeStatus[pro.id].liked ? (
-                                                    <path d="M6.956 1.745C7.021.81 7.908.087 8.864.325l.261.066c.463.116.874.456 1.012.965.22.816.533 2.511.062 4.51a10 10 0 0 1 .443-.051c.713-.065 1.669-.072 2.516.21.518.173.994.681 1.2 1.273.184.532.16 1.162-.234 1.733q.086.18.138.363c.077.27.113.567.113.856s-.036.586-.113.856c-.039.135-.09.273-.16.404.169.387.107.819-.003 1.148a3.2 3.2 0 0 1-.488.901c.054.152.076.312.076.465 0 .305-.089.625-.253.912C13.1 15.522 12.437 16 11.5 16H8c-.605 0-1.07-.081-1.466-.218a4.8 4.8 0 0 1-.97-.484l-.048-.03c-.504-.307-.999-.609-2.068-.722C2.682 14.464 2 13.846 2 13V9c0-.85.685-1.432 1.357-1.615.849-.232 1.574-.787 2.132-1.41.56-.627.914-1.28 1.039-1.639.199-.575.356-1.539.428-2.59z" />
-                                                ) : (
-                                                    <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                                                )
-                                            ) : (
-
-                                                <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2 2 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a10 10 0 0 0-.443.05 9.4 9.4 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a9 9 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.2 2.2 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.9.9 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
-                                            )
-                                            }
-                                        </svg>
-                                        <span>{pro.likeCount}</span>
-                                    </div>
+                            </Link>
+                            <div className="card-body">
+                                <h5 className="card-title h6 m-0 semi-bold-600 text-dark text-center">{pro.title}</h5>
+                                <p className="card-text pt-2 mb-1 light-300 text-dark text-center">
+                                    작성자: <UserContact nickname={pro.name} />
+                                </p>
+                                <div className='d-flex justify-content-end'>
+                                    <i
+                                        className={`bx h3 px-1 ${likeStatus[pro.id]?.liked ? 'bxs-like' : 'bx-like'} ${isHovered ? 'bx-tada' : ''}`}
+                                        onClick={(e) => handleLikeClick(e, pro.id)} // 좋아요 클릭 시 처리
+                                        style={{ color: '#4232C2' }}
+                                        onMouseEnter={() => setIsHovered(true)}
+                                        onMouseLeave={() => setIsHovered(false)}
+                                    ></i>
+                                    <span className='pt-1'style={{ color: '#4232C2' }}>{likeStatus[pro.id]?.count || pro.likeCount}</span>
                                 </div>
                             </div>
-                        </Link>
+                        </div>
                     ))}
                     <div className="form-row pt-5">
                         <div className="col-md-12 col-10 text-end">
                             {user && (
-                                <>
-                                    <Button text={'글쓰기'} onClick={() => { navigate('/project/write') }} />
-                                </>
+                                <Button text={'글쓰기'} onClick={() => navigate('/project/write')} />
                             )}
                         </div>
                     </div>
