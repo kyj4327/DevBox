b9bf3db45a1dca32b26dc03ea10ea6f88e56493d
youngjun kim <youngjun4327@gmail.com>
2024-10-05 09:15:53 +0900

    🎨style: 404페이지 디자인 수정 #259

--- devbox_client/src/components/NotFound.jsx
+++ devbox_client/src/components/NotFound.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import "./NotFound.css";
+
 
 
 function NotFound() {
   return (
 
-
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>404 - 페이지를 찾을 수 없습니다.</h1>
-      <p>요청하신 페이지는 존재하지 않습니다.</p>
-      <Link to="/">DevBox 홈으로 돌아가기</Link>
+    <div className="not-found-container">
+      <h1 className="not-found-title">404</h1>
+      <h2 className="not-found-subtitle">페이지를 찾을 수 없습니다.</h2>
+      <p className="not-found-message">요청하신 페이지는 존재하지 않습니다.</p>
+      <Link to="/" className="not-found-link">
+        DevBox 홈으로 돌아가기
+      </Link>
     </div>
   );
 }
